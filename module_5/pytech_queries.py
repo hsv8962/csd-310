654863c027fcf2d2ff32eb5382a89483261daf1a
hsv8962 <125586301+hsv8962@users.noreply.github.com>
2023-11-13 20:15:25 -0600

    Add files via upload

---
+++ module_5/pytech_queries.py
@@ -0,0 +1,28 @@
+from pymongo import MongoClient
+
+# Your MongoDB URL
+url = "mongodb+srv://hsv8962:Makework90$@cluster0.dbcpngp.mongodb.net/"
+
+# Establish a connection to the MongoDB server
+client = MongoClient(url)
+
+# Connect to the pytech database and the students collection
+db = client.pytech
+students = db.students
+
+# Find all documents in the collection
+print("-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+for student in students.find():
+    print(f"Student ID: {student['student_id']}")
+    print(f"First Name: {student['first_name']}")
+    print(f"Last Name: {student['last_name']}\n")
+
+# Find a single document using the find_one() method
+print("-- DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY --")
+bilbo = students.find_one({"student_id": "1008"})
+print(f"Student ID: {bilbo['student_id']}")
+print(f"First Name: {bilbo['first_name']}")
+print(f"Last Name: {bilbo['last_name']}")
+
+# End of program prompt
+input("End of program, press any key to continue...")
