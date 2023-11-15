03fecf6beb69a7eca4c905c9f667629ed34302f3
hsv8962 <125586301+hsv8962@users.noreply.github.com>
2023-11-15 01:38:55 -0600

    Update pytech_insert.py

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 #Mongo client database URL
-url = "mongodb+srv://hsv8962:Makework90$@cluster0.dbcpngp.mongodb.net/"
+url = "mongodb+srv://admin:admin@cluster0.dbcpngp.mongodb.net/"
 
 #Established connection to the client database
 client = MongoClient(url)
