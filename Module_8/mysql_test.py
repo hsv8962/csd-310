323f067dfd140cd274599e919ca0735f7393e5a6
hsv8962 <125586301+hsv8962@users.noreply.github.com>
2023-11-27 15:56:31 -0600

    Add files via upload

--- Module_8/mysql_test.py
+++ Module_8/mysql_test.py
@@ -1,1 +1,23 @@
+import mysql.connector
+from mysql.connector import errorcode
 
+config = {
+    'user': 'pysports_user',
+    'password':'Makework90$',
+    'host':'localhost',
+    'database': 'pysports',
+    'raise_on_warnings': True
+}
+
+try:
+    db = mysql.connector.connect(**config)
+    print("Database connection succesful!")
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Wrong username or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print ("Database not found!")
+    else:
+        print(err)
+else:
+    db.close()    
